24beefc0177739df536b7b530d224686a8b38da3
Feenny <kolomievskayazhenya@gmail.com>
2024-06-20 01:04:44 +0300

    правки поиска, верстка списка чатов

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 export { Button } from './button';
+export { BurgerMenu } from './burger-menu';
 export { ChatItem } from './chat-item';
 export { ChatList } from './chat-list';
 export { ChatSearch } from './chat-search';
 export { ChatContainer } from './chat-container';
 export { Dialog } from './dialog';
+export { Header } from './header';
 export { Input } from './input';
 export { InputField } from './input-field';
 export { Link } from './link';
