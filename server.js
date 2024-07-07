1bf03e1b2dc95ffc2eb74798ab22b09a452ee289
Feenny <kolomievskayazhenya@gmail.com>
2024-07-08 02:11:39 +0300

    changhe eslint config

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-// import express from "express"
-// import path from "path"
+import express from "express"
+import path from "path"
 
-// const app = express()
-// const PORT = 3000
+const app = express()
+const PORT = 3000
 
-// app.use(express.static("dist"))
+app.use(express.static("dist"))
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../assets/index.html"))
-// })
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../assets/index.html"))
+})
 
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`)
-// })
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
