8790930066d34c7ff1900315c0238516736d20ef
Feenny <kolomievskayazhenya@gmail.com>
2024-07-08 00:38:52 +0300

    add stylelint commands

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('dist'));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../assets/index.html"));
-}); 
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
