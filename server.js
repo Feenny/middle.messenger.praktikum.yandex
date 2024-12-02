c9d7e20d8802adee6aa5fbeda32940d316fdb84e
Feenny <kolomievskayazhenya@gmail.com>
2024-12-02 23:22:26 +0300

    newline

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ const server = http.createServer(app);
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
-app.use(express.static('dist'))
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../assets/index.html'))
-})
-
+app.use(express.static('./dist'));
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(dirname, 'dist/index.html'));
+});
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
