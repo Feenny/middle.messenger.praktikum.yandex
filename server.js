b2685a0449ef5eadbf9d51315ed0688700421426
Feenny <kolomievskayazhenya@gmail.com>
2024-12-02 23:17:43 +0300

    newline

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import express from 'express'
+import http from 'http'
 import path from 'path'
 
 const app = express()
 const PORT = 3000
+const server = http.createServer(app);
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 app.use(express.static('dist'))
 
@@ -10,6 +14,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../assets/index.html'))
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
