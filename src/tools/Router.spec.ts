a1c5b1a1e28fd82737d9c68e9973840381b984a9
Feenny <kolomievskayazhenya@gmail.com>
2024-12-07 23:38:55 +0300

    тесты для роутера

--- src/tools/Router.spec.ts
+++ src/tools/Router.spec.ts
@@ -1,12 +1,20 @@
-// import { expect } from 'chai';
-// import Router from './Router';
-// import Block from './Block';
+/* eslint-disable no-unused-expressions */
+import { expect } from 'chai';
+import Sinon from 'sinon';
+import Block from './Block.ts';
+import Router from './Router.ts';
 
-// const router = new Router('app');
+const router = new Router('app');
 
-// describe('Router', () => {
-//     it('Найти роут', () => {
-//         router.use('/404', Block);
-//         expect(router.getRoute('/404') !== undefined).to.eq(true);
-//     });
-// });
+describe('Router', () => {
+    it('Роут создан', () => {
+        const spy = Sinon.spy(router, 'start');
+        router.start();
+
+        expect(spy.calledOnce).to.be.true;
+    });
+    it('Роут найден', () => {
+        router.use('/404', Block);
+        expect(router.getRoute('/404') !== undefined).to.eq(true);
+    });
+});
